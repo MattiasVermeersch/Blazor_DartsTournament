89c58467adf3e4a29608ebfb5e5c5b03428de37a
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-11-12 17:35:32 +0100

    Create PlayerStatistics class

---
+++ src/Pin.DartsTournament.Core/Entities/PlayerStatistics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.DartsTournament.Core.Entities
+{
+    public class PlayerStatistics : BaseEntity
+    {
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+        public int NumberOfTriples { get; set; }
+        public int NumberOfDoubles { get; set; }
+        public Player Player { get; set; }
+    }
+}
