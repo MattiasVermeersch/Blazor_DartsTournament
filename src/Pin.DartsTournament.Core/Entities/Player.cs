8bea037991ed1576a98ece7894c988ee11817260
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2021-11-12 17:32:17 +0100

    Create Player class

---
+++ src/Pin.DartsTournament.Core/Entities/Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.DartsTournament.Core.Entities
+{
+    public class Player : BaseEntity
+    {
+        public string Name { get; set; }
+        public PlayerStatistics Statistics { get; set; }
+    }
+}
