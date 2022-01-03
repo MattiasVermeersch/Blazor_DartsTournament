12948deea98f6dcf0108f633a1f91350c7c34b9a
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-03 22:31:12 +0100

    Create Leg class

---
+++ src/Pin.DartsTournament.Core/Entities/Leg.cs
@@ -0,0 +1,9 @@
+﻿namespace Pin.DartsTournament.Core.Entities
+{
+    public class Leg : EntityBase
+    {
+        public int Score { get; set; }
+        public Player Player { get; set; }
+        public IEnumerable<Dart> Throws { get; set; }
+    }
+}
