15bdd55bc326fa8c4a53432344fc6dd224972bdf
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-03 22:29:46 +0100

    Create Tournament class

---
+++ src/Pin.DartsTournament.Core/Entities/Tournament.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.DartsTournament.Core.Entities
+{
+    public class Tournament : EntityBase
+    {
+        public IEnumerable<Player> Players { get; set; }
+        public IEnumerable<Game> Games { get; set; }
+        public IEnumerable<Referee> Referees { get; set; }
+    }
+}
