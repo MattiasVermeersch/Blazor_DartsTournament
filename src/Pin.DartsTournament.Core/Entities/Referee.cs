48174df957a11f2a21d29eb0603f6f0ac9535346
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-03 22:31:31 +0100

    Create Referee class

---
+++ src/Pin.DartsTournament.Core/Entities/Referee.cs
@@ -0,0 +1,9 @@
+﻿namespace Pin.DartsTournament.Core.Entities
+{
+    public class Referee : EntityBase 
+    {
+        public string Name { get; set; }    
+        public Game Game { get; set; }  
+        public IEnumerable<Game> Games { get; set; }    
+    }
+}
