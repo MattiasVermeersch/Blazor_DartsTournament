6661f40cfae340fa3e7e1024e9836f084b2ab6a3
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-06 20:58:36 +0100

    Create PlayerGame class for many to many relation

---
+++ src/Pin.DartsTournament.Core/Entities/PlayerGame.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.DartsTournament.Core.Entities
+{
+    public class PlayerGame
+    {
+        public long PlayerId { get; set; }
+        public long GameId { get; set; }
+        public Player Player { get; set; }
+        public Game Game { get; set; }
+    }
+}
