01d6f9e41f61aeeb67f9a1ad6e48358c94fef356
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-06 21:00:41 +0100

    Update and rename Dart class to Throw

--- src/Pin.DartsTournament.Core/Entities/Throw.cs
+++ src/Pin.DartsTournament.Core/Entities/Throw.cs
@@ -1,6 +1,6 @@
 ﻿namespace Pin.DartsTournament.Core.Entities
 {
-    public class Dart : EntityBase
+    public class Throw : EntityBase
     {
         public string Type { get; set; }
         public int Number { get; set; }
