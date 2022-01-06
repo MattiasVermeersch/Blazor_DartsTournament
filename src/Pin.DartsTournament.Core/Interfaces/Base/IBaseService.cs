e8b1adaaee0b031dc3b2360f99f0f695c6b44b18
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-06 21:06:55 +0100

    Create IBaseService interface

---
+++ src/Pin.DartsTournament.Core/Interfaces/Base/IBaseService.cs
@@ -0,0 +1,20 @@
+﻿using Pin.DartsTournament.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.DartsTournament.Core.Interfaces
+{
+    public interface IBaseService<T> where T : EntityBase
+    {
+        IQueryable<T> GetAllAsync();
+        Task<IEnumerable<T>> ListAllAsync();
+        Task<T> GetByIdAsync(long id);
+        Task<bool> AddAsync(T entity);
+        Task<bool> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(T entity);
+        Task<bool> DeleteByIdAsync(long id);
+    }
+}
