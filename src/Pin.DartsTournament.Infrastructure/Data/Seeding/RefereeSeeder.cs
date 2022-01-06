bf62fd10156e28e2c1357a50448d335b11328ce1
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-06 21:03:43 +0100

    Create RefereeSeeder

---
+++ src/Pin.DartsTournament.Infrastructure/Data/Seeding/RefereeSeeder.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pin.DartsTournament.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.DartsTournament.Infrastructure.Data.Seeding
+{
+    public class RefereeSeeder
+    {
+        public static void Seed(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Referee>().HasData(
+                new Referee
+                {
+                    Id = 30,
+                    Name = "Lector Deboosere"
+                },
+                new Referee
+                {
+                    Id = 31,
+                    Name = "Lector Derdeyn"
+                }
+            );
+        }
+    }
+}
