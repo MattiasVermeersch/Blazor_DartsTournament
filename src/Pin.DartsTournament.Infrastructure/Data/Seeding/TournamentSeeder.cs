2dc5e68aaf5658f8df952fdae7dfafbd55b65662
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-06 21:03:54 +0100

    Create TournamentSeeder

---
+++ src/Pin.DartsTournament.Infrastructure/Data/Seeding/TournamentSeeder.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pin.DartsTournament.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.DartsTournament.Infrastructure.Data.Seeding
+{
+    public class TournamentSeeder
+    {
+        public static void Seed(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Tournament>().HasData(
+                new Tournament
+                {
+                    Id = 100,
+                    Name = "Conf-IT-uurtjes Toernooi"
+                }    
+            );
+        }
+    }
+}
