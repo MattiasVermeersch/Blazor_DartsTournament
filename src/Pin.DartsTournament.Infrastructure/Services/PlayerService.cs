6f3979bc9a3b2326a1f88e3908090de4afb1ae22
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-06 21:13:57 +0100

    Create PlayerService class

---
+++ src/Pin.DartsTournament.Infrastructure/Services/PlayerService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pin.DartsTournament.Core.Entities;
+using Pin.DartsTournament.Core.Interfaces;
+using Pin.DartsTournament.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pin.DartsTournament.Infrastructure.Services
+{
+    public class PlayerService : BaseService<Player>, IPlayerService
+    {
+        public PlayerService(DartsDbContext dbContext) : base(dbContext) 
+        { 
+        }
+
+        public override IQueryable<Player> GetAllAsync()
+        {
+            return _dbContext.Players.AsNoTracking()
+                .Include(p => p.PlayerGames)
+                    .ThenInclude(pg => pg.Game);
+        }
+
+        public override async Task<Player> GetByIdAsync(long id)
+        {
+            return await GetAllAsync().SingleOrDefaultAsync(p => p.Id.Equals(id));
+        }
+
+        public async Task<Player> GetPlayerByName(string name)
+        {
+            return await GetAllAsync().FirstOrDefaultAsync(p => p.Name.Equals(name));
+        }
+    }
+}
