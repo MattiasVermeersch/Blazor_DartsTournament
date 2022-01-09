5ea6a9249c28b35affd1daba41ab57189d046a01
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-09 02:55:03 +0100

    Create IBaseService for blazor project

---
+++ src/Pin.DartsTournament.Blazor/Core/Interfaces/Base/IBaseService.cs
@@ -0,0 +1,11 @@
+﻿namespace Pin.DartsTournament.Blazor.Interfaces
+{
+    public interface IBaseService<T> where T : class
+    {
+        Task<IEnumerable<T>> ListAllAsync();
+        Task<T> GetByIdAsync(long? id);
+        Task<T> AddAsync(T entity);
+        Task<T> UpdateAsync(T entity); 
+        Task<bool> DeleteAsync(long id);
+    }
+}
