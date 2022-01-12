60a52e451cac49ab69297a8a0ac64383095d984f
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-12 21:27:00 +0100

    Update migrations

--- src/Pin.DartsTournament.Infrastructure/Migrations/20220112195839_FixTournamentEntity.Designer.cs
+++ src/Pin.DartsTournament.Infrastructure/Migrations/20220112195839_FixTournamentEntity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Pin.DartsTournament.Infrastructure.Data;
 
@@ -11,9 +12,10 @@ using Pin.DartsTournament.Infrastructure.Data;
 namespace Pin.DartsTournament.Infrastructure.Migrations
 {
     [DbContext(typeof(DartsDbContext))]
-    partial class DartsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220112195839_FixTournamentEntity")]
+    partial class FixTournamentEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Pin.DartsTournament.Infrastructure/Migrations/20220112195839_FixTournamentEntity.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Pin.DartsTournament.Infrastructure.Migrations
+{
+    public partial class FixTournamentEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
