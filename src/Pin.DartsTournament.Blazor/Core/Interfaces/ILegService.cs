2679f4a5115f84866492eb4c1ef2c78f7ddad9ec
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-15 02:04:28 +0100

    Update LegService interface

---
+++ src/Pin.DartsTournament.Blazor/Core/Interfaces/ILegService.cs
@@ -0,0 +1,10 @@
+﻿using Pin.DartsTournament.Core.Entities;
+
+namespace Pin.DartsTournament.Blazor.Interfaces
+{
+    public interface ILegService : IBaseService<Leg>
+    {
+        Task<Leg> StartMatch(long id);
+        Task SubmitSet(IEnumerable<Throw> thrown, long? legId, long? playerId);
+    }
+}
