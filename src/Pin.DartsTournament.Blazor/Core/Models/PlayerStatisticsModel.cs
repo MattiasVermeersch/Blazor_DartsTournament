ff1f438f0378db671d24050497fa6bac2d915cbd
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-16 23:05:18 +0100

    Create PlayerStatisticsModel class

---
+++ src/Pin.DartsTournament.Blazor/Core/Models/PlayerStatisticsModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Pin.DartsTournament.Blazor.Models
+{
+    public class PlayerStatisticsModel
+    {
+        public string PlayerName { get; set; }
+        public int? Wins { get; set; }
+        public int? Losses { get; set; }
+        public int? TypeCountT20 { get; set; }
+        public int? NumberOfTrebles { get; set; }
+        public int? NumberOfDoubles { get; set; }
+        public double AverageScore { get; set; }
+    }
+}
