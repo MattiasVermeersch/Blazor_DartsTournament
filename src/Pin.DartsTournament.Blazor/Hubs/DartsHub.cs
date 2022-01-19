5c937cf8185df6833026de049ce479306b7a531d
MattiasVermeersch <mattias.vermeersch@student.howest.be>
2022-01-19 22:45:07 +0100

    Create DartsHub class

---
+++ src/Pin.DartsTournament.Blazor/Hubs/DartsHub.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Pin.DartsTournament.Blazor.Hubs
+{
+    public class DartsHub : Hub
+    {
+        public async Task SendTournament()
+        {
+            await Clients.All.SendAsync("GetTournament");
+        }
+
+        public async Task SendResolvedLegData()
+        {
+            await Clients.All.SendAsync("GetStatistics");
+        }
+
+        public async Task SendLegData()
+        {
+            await Clients.All.SendAsync("GetLegData");
+        }
+    }
+}
